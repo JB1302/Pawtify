0795d4d58827b873befe6dbeae6e2886e1ea273f
JB1302 <jstevenbarrantes@gmail.com>
2025-07-02 22:59:12 -0600

    Inicio del Proyecto

---
+++ src/test/java/com/Pawtify/pawtify/PawtifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pawtify.pawtify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawtifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
