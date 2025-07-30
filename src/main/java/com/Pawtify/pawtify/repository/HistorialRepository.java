767b8f973f4ac83c1e390389e75b709cccbf5cd4
Alejandro <Alejandro@DESKTOP-FQ46T3T>
2025-07-29 21:37:39 -0600

    Pawtify filtros

--- src/main/java/com/Pawtify/pawtify/repository/HistorialRepository.java
+++ src/main/java/com/Pawtify/pawtify/repository/HistorialRepository.java
@@ -5,12 +5,18 @@
 package com.Pawtify.pawtify.repository;
 
 import com.Pawtify.pawtify.domain.Historial;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author jstev
  */
 public interface HistorialRepository extends JpaRepository<Historial, Long>{
-    
+     @Query(nativeQuery = true,
+            value="SELECT * FROM stock_historial a WHERE a.id_producto LIKE CONCAT('%', :texto, '%') OR a.cambio LIKE CONCAT('%', :texto '%') OR a.motivo LIKE CONCAT('%', :texto, '%')OR a.fecha LIKE CONCAT('%', :texto '%')order by id"
+     )
+   
+    public List<Historial> ConsultaSQL(String texto);
 }
